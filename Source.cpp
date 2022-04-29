c9f18e37dd4fab6ed8a7919b401ae99c38ed38f1
salma1608 <97280135+salma1608@users.noreply.github.com>
2022-04-29 11:55:15 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,16 @@
 #include <string>
 #include <regex>
 #include <windows.h>
+#include <fstream>
 using namespace std;
 int main()
 {
+    ofstream file;
+    file.open("Register.txt");
     string email;
     cout << "Enter The Email: ";
     getline(std::cin, email);
-    cout << "Enter the password : ";
+    cout << "Enter The Password : ";
     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
     DWORD mode = 0;
     GetConsoleMode(hStdin, &mode);
@@ -18,9 +21,14 @@ int main()
     getline(cin, s);
     cout << endl;
     string word = "@oi.eg";
-    regex fox_re( word, regex_constants::icase);
-    if (regex_search(email, fox_re)&& islower(s[s.size()]) + isupper(s[s.size() - 1]))
-        cout << "valid" << "\n";
-    else
+    regex w( word , regex_constants::icase);
+    if (regex_search(email, w) && islower(s[s.size()]) + isupper(s[s.size() - 1])) {
+        cout << "valid... :)" << "\n";
+        file << email << endl << s << endl;
+    }
+    else {
         cout << "invalid... :(";
+    }
+    file.close();
+    return 0;
 }
